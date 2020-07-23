1a5caf61620528ef6135ffa816518908ecae5417
rcedras0010@gmail.com <ronaldocedras@gmail.com>
2020-07-23 15:58:42 +0200

    added flex besigheid

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import Flask, render_template
+import random
 
 @app.route('/')
 def index():
@@ -10,64 +11,49 @@ def about():
     path = "static/images/"
     dsc = "https://www.linkedin.com/company/dscuct/"
     ronaldo = "Ronaldo Cedras, is a third year computer science student at Stellenbosch University. Since a young age, he has shown interest in computers and making it do things. I joined DSC on a quest to better myself as a developer and becoming a more well-rounded software engineer. As a tech mentor I hope to help mould students to exactly this. In his free time he watches animated philosophy on YouTube, clicks on heads in Rainbow Six Siege and surfs the internet."
-
+    suvana = "Suvana, who is one of our Tech mentors, is a full-time student at the university of Cape Town. She is in her final year of study in Electrical and Computer Engineering. Suvana joined the DSC to put in practice the theory she learnt on campus and to help others. “The community space was filled with exciting people who were eager to take on real problems”"
+    
     ls = [
-    {
-    'name': 'Wavhudi Beckham',
-    'photo': path+"Wavhudi.png",
-    'intro': "Wavhudi Cornelius Mulaudzi is a second-year Computer Science and Computer Engineering student at the University Of Cape Town. His role at the DSC club is Tech mentor. He is passionate about coding and solving socio-economic problems through the application of Information Technology. He hopes to achieve his dream of becoming a software developer and owning his own Tech company.",
-    },
-    {
-    'name': 'Ryan Cecrest',
-    'photo': path+"Ryan.png",
-    'intro': "Ryan joined the DSC to become part of a community that supports and encourages the development of ambitious software develops who want to make an impact on the world. He wants to help students to get the most out of there DSC experience while having fun and learning new concepts along the way."
-    },
-    {
-    'name': 'Ronaldo Seegras',
-    'photo': path+"Ronaldo.png",
+{
+    'name': "Ronaldo Cedras",
     'git': "https://github.com/ronaldo0010",
     'web': "https://rcedras.herokuapp.com/",
     'role': "Web & Mobile apps",
-    'linked': "https://www.linkedin.com/in/ronaldo-cedras-80b7a9162/",
     'lang': "Python, Java, JS, Dart, Html, CSS",
     'intro': ronaldo,
-
-    },
-    {
-    'name': 'Richard Dawson',
-    'photo': path+"Richard.png",
-    'git': "https://github.com/ronaldo0010",
+    'photo':"https://media-exp1.licdn.com/dms/image/C5603AQETGGYPsRiQ1w/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=lkmMVZGV8blloCMovclR0ZvAKHZeZbhpfiJBPrZrtZk",
     'linked': "https://www.linkedin.com/in/ronaldo-cedras-80b7a9162/",
-    'web': "https://rcedras.herokuapp.com/",
-    'intro': "Richard makes marketing materials for both print and web, and joined the Google DSC to bring hopeful coders closer to the tech industry. He’s deeply invested in the intersection between technology and business, and has previous experience in the fields of biomedical engineering, higher education, web development and graphic design, and screenwriting",
-    },
-    {
-    'name': 'Nkosi Gates',
-    'photo': path+"Nkosi.png",
-    'git': "https://github.com/ronaldo0010",
-    'linked': "https://www.linkedin.com/in/ronaldo-cedras-80b7a9162/",
-    'web': "https://rcedras.herokuapp.com/",
-    'intro': "Nkosi is a Part-time student at the University of London majoring in Computer Science : Games Development and a Test Engineer.",
-    },
-    {
-    'name': 'Jonathan De Wet',
-    'photo': path+"Jonathan_dk.png",
-    'git': "https://github.com/ronaldo0010",
-    'linked': "https://www.linkedin.com/in/ronaldo-cedras-80b7a9162/",
-    'web': "https://rcedras.herokuapp.com/",
-    'intro': "Jonathan de Kock is a professional full-stack developer who forms part of the Google DSC as a result of his love for teaching eager minds how to think like programmers, solve real-world challenges by being innovative and how to flourish in the tech industry. He is a self taught developer with a focus on building powerful cross-platform applications for listed companies using web technologies",
-    },
-    {
+},
+{
     'name': "George Rautenbach",
     'git': "https://github.com/Koellewe",
     'web': "https://rauten.co.za/",
-    'photo': path+"George.png",
     'role': "Computer Ethics, DevOps, SysAdmin",
     'lang': "Bash, Go, Python, SQL, PHP",
     'linked': "https://www.linkedin.com/in/jans-rautenbach/",
-    'intro': "George is a final year Computer Science & Philosophy student interested in the intersection between these fields. When he isn’t working on personal and collaborative tech projects, he blogs about computer ethics, AI identity, and other philosophical ideas. ",
+    'photo':"https://media-exp1.licdn.com/dms/image/C5603AQEHFxW-n9eV3g/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=xGVFAWyL9t_rJBickEE6BzG0cP_ZqXWT2XeDpAAbTzE",
+    'intro': suvana,
+},
+{
+    'name': "Suvana Rohanlal",
+    'git': "https://github.com/Suvana-Rohanlal",
+    'role': "Web & Mobile Apps, Embedded Systems",
+    'lang': "Java, Python, HTML, CSS, JS,C++",
+    'linked': "www.linkedin.com/in/suvana-rohanlal",
+    'photo':"https://media-exp1.licdn.com/dms/image/C4D03AQGBfK_IVkjZwQ/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=lgq6GPibLubIjwscuWsoNvCQT_cjE5Q6HwiLBefIJtI",
+    'intro': suvana,
+},
 
-    }
+{
+    'name': "Nkanyiso Sigwaza",
+    'git': "https://github.com/NkanyisoSigwaza",
+    'role': "Mobile Apps",
+    'lang': "Java, Python, HTML, CSS, JS, C++, Dart, Flutter, SQL",
+    'linked': "https://www.linkedin.com/in/nkanyiso-sigwaza-57734a131/",
+    'photo':"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    'intro': suvana,
+},
+
     ]
     for i in range(len(ls)):
         if 'linked' not in ls[i]:
@@ -82,6 +68,9 @@ def about():
             ls[i]['intro'] = ' '
         if 'lang' not in ls[i]:
             ls[i]['lang'] = ' '
-        ls[i]['mid'] = str(i * 3)
+        ls[i]['mid'] = mid_gen()
 
     return render_template('about.html', ls=ls, stop=len(ls))
+
+def mid_gen():
+    return str(random.randint(10,500))
